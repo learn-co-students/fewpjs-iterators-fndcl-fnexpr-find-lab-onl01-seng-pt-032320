5bcaa4e0359dbfa99ed792eeef087dcbe7a8a445
Abdul Khan <akhan17@outlook.com>
2020-09-22 20:24:02 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(theArrayProvided) {
+  const win = theArrayProvided.find(obj => obj.result === "W");
+
+  if (win === undefined) {
+  	return undefined;
+  } else {
+  	return win.year;
+  }
+}
