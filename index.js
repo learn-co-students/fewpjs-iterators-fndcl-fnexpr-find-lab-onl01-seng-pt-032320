71f3cfd4041774f7a88893ae605864999fc7648e
curtis <laurencechadwell@gmail.com>
2020-09-27 06:49:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
+const record =[ 
+  {year: "1969", result: 'W'},
+  {year: "2016", result: 'N/A'},
+  {year: "2017", result: 'N/A'},
+  {year: "2018", result: 'N/A'},
+  {year: "2019", result: 'W'}
+]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let outcome = record.find(record => record.result === "W")
+  return outcome ? outcome.year : undefined
 }
