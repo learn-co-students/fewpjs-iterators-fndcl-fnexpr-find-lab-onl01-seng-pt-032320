b452bd805e783c0c25bafc157f6f0d57bc2592f6
HMoreira2020 <moreiraheather@gmail.com>
2020-09-29 21:40:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  let result = array.find(function(e) { return e["result"] === "W"})
+  if (result) { return result.year} 
 }
+
+
+
