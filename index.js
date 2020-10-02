1605421147b44cdeebacbdc1697abcf267b00ba1
arbrown110 <arbrown110@gmail.com>
2020-10-02 17:34:33 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = function(record) {
+  let champ = record.find(record => record.result === "W");
+    return champ? champ.year :undefined
+}
