e2553d7999f06799c43a4fa0be9af37392e85f9f
nabilhayet <nabil4457@yahoo.com>
2020-10-08 15:38:56 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+superbowlWin(record);
+
+function superbowlWin(arrObject){
+  let value = arrObject.find(function(s) { 
+    return s.result === "W"
+  })
+
+  console.log(value);
+
+  return value ? value.year : undefined
+  
+}
