4050268c7e18f90ab47c902e1b7f170b26bdf3de
megfly <mflynn333@gmail.com>
2020-10-09 13:05:16 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  //console.log(team)
+  //let win = record.find( function(result) { return result === "W"} )
+  let win = record.find(team => team.result === "W")
+  
+  if (win === undefined) {
+    return undefined
+  } else {
+    return win.year
+  } 
+}
