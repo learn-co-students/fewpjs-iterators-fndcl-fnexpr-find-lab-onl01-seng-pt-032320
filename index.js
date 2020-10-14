0cb5e4076071a5fa2a2aa91161c7dd4ea24fc363
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-13 19:58:52 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records) {
+  let winner = records.find(function(rec) { return rec.result === "W" });
+  return (winner ? winner.year : winner);
+}
