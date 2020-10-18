0d11bd7622e8e2ea326daf6e2ec8b9a23880a9d3
omarnabi1 <onabi20@gmail.com>
2020-10-18 15:14:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let outcome = record.find(record => record.result === "W")
+  return outcome ? outcome.year : undefined
 }
